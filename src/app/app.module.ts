6cc7309db87676dcee728b4597a248cd8daec3a7
Azizbek <a.botirov@rtsb.uz>
2025-03-17 13:08:04 +0500

    home page layout ready

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './pages/public/home-page/home-page.component';
+import { HeaderComponent } from './pages/public/elements/header/header.component';
+import { FooterComponent } from './pages/public/elements/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
